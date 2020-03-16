ca4efb96c3cdb4104b766082f638810d3244cdbb
StefanLee <1693625254@qq.com>
2020-03-17 00:26:29 +0800

    test

--- my_site/wsgi.py
+++ my_site/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling #导入了帮助正确提供静态文件的Cling，并使用它来启动应用程序
+# from dj_static import Cling #导入了帮助正确提供静态文件的Cling，并使用它来启动应用程序
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_site.settings')
 
 application = get_wsgi_application()
