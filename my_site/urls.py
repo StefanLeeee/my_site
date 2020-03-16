a05790cef775bd1a68fe854e7b757ecdc3a4ebd3
StefanLee <1693625254@qq.com>
2020-03-16 14:46:24 +0800

    first

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog import views
 urlpatterns = [
+    path('index/',views.index),
     path('admin/', admin.site.urls),
 ]
