f0aa268d6020af22d1880a99833ff213b2a37677
StefanLee <1693625254@qq.com>
2020-03-17 00:19:33 +0800

    fix some bugs about cannot open app

--- my_site/urls.py
+++ my_site/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from blog import views
 urlpatterns = [
-    path('',views.index),
+    path('index/',views.index),
     path('admin/', admin.site.urls),
 ]
